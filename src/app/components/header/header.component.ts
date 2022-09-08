8603af8cf8f69f33b568b63364c145fbf2f720f3
Luciana <lucianatux@yahoo.com.ar>
2022-09-07 22:29:50 -0300

    bootstrap

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from 'src/app/model/persona';
+import { PersonaService } from 'src/app/services/persona.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  persona: Persona = new Persona("", "", "", "", "", "", "", "", "", "", "");
+  isLogged = false;
 
-  constructor() { }
+  constructor(public personaService: PersonaService, 
+    private router: Router, 
+    private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data=>{this.persona = data});
+    if(this.tokenService.getToken()) {
+      this.isLogged = true;
+    }else{
+      this.isLogged = false;
+    }
   }
 
-}
+  login(){
+    this.router.navigate(['/login'])
+  }
+
+  onLogOut(): void {
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
+  }
+
